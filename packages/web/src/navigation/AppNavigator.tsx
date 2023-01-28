55b628a6efa14827095b23d37fbd3c55e698eedb
JPychon <64781464+JPychon@users.noreply.github.com>
2023-01-27 22:16:58 -0800

    Fixed the login button.

--- packages/web/src/navigation/AppNavigator.tsx
+++ packages/web/src/navigation/AppNavigator.tsx
@@ -1,19 +1,22 @@
-import React, {useState} from "react";
-import { Button, Icon, useColorMode, useColorModeValue } from "native-base";
+/**
+ * Contains the application navigational components. 
+ * @author Elias Schablowski & John Hanna
+ * @format
+ */
+
+import React from "react";
+import { Button, Icon, useColorMode, useColorModeValue, Spinner } from "native-base";
+import { Login, SignUp, Estimates, Request } from "@RideSaver/screens";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import { CustomDrawer } from "@RideSaver/components";
+import { NavigationTheme } from "../theme";
+import { useSelector } from "react-redux";
+import { user } from "@RideSaver/store";
 import { useLingui } from "@lingui/react";
 import { t } from "@lingui/macro";
-
-import { Login } from "@RideSaver/screens";
-import { SignUp } from "@RideSaver/screens";
-import { Estimates } from "@RideSaver/screens";
-import { Request } from "@RideSaver/screens";
-import { createStackNavigator } from "@react-navigation/stack";
-import {  NavigationContainer } from "@react-navigation/native";
-import { NavigationTheme } from "../theme";
 import { i18n } from "@lingui/core";
-
 
 const EstimateStack = createStackNavigator(); // Estimates Screen
 function EstimateStackScreen() { 
@@ -39,36 +42,42 @@ function LoginStackScreen() {
             </LoginStack.Navigator>
         );}
     
-const SignupStack = createStackNavigator(); // Signup Screen
-function SignupStackScreen() { 
+const RegisterStack = createStackNavigator(); // Registration Screen
+function RegisterStackScreen() { 
     return( 
-            <SignupStack.Navigator screenOptions={{ headerShown: false }}>
-                <SignupStack.Screen name="SignUp" component={SignUp} />
-            </SignupStack.Navigator>
+            <RegisterStack.Navigator screenOptions={{ headerShown: false }}>
+                <RegisterStack.Screen name="SignUp" component={SignUp} />
+            </RegisterStack.Navigator>
         );}
     
-const RootDrawer = createDrawerNavigator(); // Root Drawer Navigator
-function HomeTabs( {token} : {token: boolean} ) {
+const RootDrawer = createDrawerNavigator(); // Primary/Root Drawer Navigator
+function HomeTabs() 
+    {
         
+        const isLoading = useSelector(user.getIsLoading) as boolean;
+        const token = useSelector(user.getToken) as string;
         const { toggleColorMode } = useColorMode();
         const colorMode = useColorModeValue("light", "dark");
         const { i18n } = useLingui();
 
-return( <RootDrawer.Navigator
-                useLegacyImplementation
-                initialRouteName={token === undefined ? "Login" : "Estimates"}
-                screenOptions={({ navigation }) => ({ headerLeft: () => ( <Icon name="menu" onPress={() => navigation.openDrawer()} />), })}       
-                drawerContent={(props) => ( <CustomDrawer {...props} toggleColorMode={toggleColorMode} colorMode={colorMode} />)}>
+        if(isLoading) { return <Spinner/>; }
+
+        return( 
+        <RootDrawer.Navigator
+                    useLegacyImplementation
+                    initialRouteName={token === undefined ? "Login" : "Estimates"}
+                    screenOptions={({ navigation }) => ({ headerLeft: () => ( <Icon name="menu" onPress={() => navigation.openDrawer()}/>)})}       
+                    drawerContent={(props) => ( <CustomDrawer {...props} toggleColorMode={toggleColorMode} colorMode={colorMode}/>)}>
 
                                     {token === undefined ? (<RootDrawer.Group navigationKey={"guest"}>
             
-                        <RootDrawer.Screen name="Login" component={LoginStackScreen} options={{ title: t(i18n)`Login` }}/>
-                        <RootDrawer.Screen name="SignUp" component={SignupStackScreen} options={{title: t(i18n)`Register` }}/>
+                        <RootDrawer.Screen name="Login" component={LoginStackScreen} options={{ title: t(i18n)`Login`}}/>
+                        <RootDrawer.Screen name="SignUp" component={RegisterStackScreen} options={{title: t(i18n)`Register`}}/>
                   
                                         </RootDrawer.Group>):(<RootDrawer.Group navigationKey="{user}">
                                             
-                        <RootDrawer.Screen name="Estimates" component={EstimateStackScreen} options={{ title: t(i18n)`Rides & Services`}} />
-                        <RootDrawer.Screen name="Request" component={RequestStackScreen} options={{ title: t(i18n)`Ride Details`}}  />
+                        <RootDrawer.Screen name="Estimates" component={EstimateStackScreen} options={{ title: t(i18n)`Rides & Services`}}/>
+                        <RootDrawer.Screen name="Request" component={RequestStackScreen} options={{ title: t(i18n)`Ride Details`}} />
 
                                                             </RootDrawer.Group>)}
         
@@ -76,10 +85,9 @@ return( <RootDrawer.Navigator
     )
 }
 
-const RootStack = createStackNavigator(); // Root Stack Navigator
+const RootStack = createStackNavigator(); // Primary/Root Stack Navigator
 export default function AppNavigator({ token } : {token : boolean} ) {
 
-    const [userToken, setUserToken] = useState(token); // TODO -> Update user JWT token for nav-groups.
     const navigationTheme = useColorModeValue( NavigationTheme.Light, NavigationTheme.Dark);
 
     return( 
@@ -91,7 +99,7 @@ export default function AppNavigator({ token } : {token : boolean} ) {
                             <RootStack.Screen 
                             name="HomeTabs" 
                             component={HomeTabs}  
-                            options= {({ route, navigation }) =>  ({ headerLeft: () => <Button onPress={navigation.navigate(route, { token: {userToken}})}/>})}
+                            options= {({ route, navigation }) =>  ({ headerLeft: () => <Button onPress={navigation.navigate(route)}/>})}
                             />
                 </RootStack.Navigator>
                 
